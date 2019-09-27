8751569979cb79b094a41f70eda5d8ffe318e4e7
Robert Gutierrez <robertg@cs.utexas.edu>
2019-09-27 18:04:18 -0500

    Started working on the about page.

--- src/About.js
+++ src/About.js
@@ -1,8 +1,101 @@
 import React from 'react'
+import { Jumbotron } from './components/Jumbotron'
 
 export const About = () => (
-  <div>
-    <h2>About Page</h2>
-    <p>This is mostly about us.</p>
-  </div>
+
+  <main role="main">
+
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">Meet the Team</h1>
+        <p class="lead text-muted">Get to know the team responsible for creating and maintaining this site!</p>
+      </div>
+    </section>
+
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <h5>Robert C. Gutierrez</h5>
+                  <p class="card-text">I like to box, lift, and code.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <h5>Calvin Dao</h5>
+                  <p class="card-text">Filler text</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <h5>David Sikabwe</h5>
+                  <p class="card-text">Filler text.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <h5>Abu-Zayd Abdullah</h5>
+                  <p class="card-text">Filler text.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <h5>Ben Hazel</h5>
+                  <p class="card-text">Filler text.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <h5>Ben Burton</h5>
+                  <p class="card-text">Filler text.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+  </main>
+
 )
