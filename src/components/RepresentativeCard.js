221c2efe8aecf6376493a1d0a7f9fff21fb2d56c
Robert Gutierrez <robertg@cs.utexas.edu>
2019-09-29 14:29:00 -0500

    Changed politiciancard to representativecard.

--- src/components/RepresentativeCard.js
+++ src/components/RepresentativeCard.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
-import data from "../data/PoliticalMembers"
+import data from "../data/RepresentativeMembers"
 
-class PoliticianCard extends Component {
+class RepresentativeCard extends Component {
   render() {
-  const polList = data.map(politician => {
+  const repList = data.map(representative => {
   return (
-  <div className="col-md-4" key={politician.index}>
+  <div className="col-md-4" key={representative.index}>
   <div className="card mb-4 box-shadow">
-    <img className="card-img-top about-image" style={{maxHeight: 262}} src={politician.image} alt="Card image cap"></img>
+    <img className="card-img-top about-image" style={{maxHeight: 262}} src={representative.image} alt="Card image cap"></img>
     <div className="card-body">
-      <h5>{politician.name}</h5>
+      <h5>{representative.name}</h5>
       <p>
       <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis feugiat elit, ac porta diam commodo non. In pulvinar neque dolor, sed vestibulum odio dapibus a. Curabitur turpis lacus, commodo eu purus sit amet, venenatis tristique ex. Donec sit amet mattis tellus, non hendrerit urna.</p>
       </p>
@@ -26,11 +26,11 @@ class PoliticianCard extends Component {
   )
   }
     )
-  return polList
+  return repList
 }
 }
 
-export default PoliticianCard;
+export default RepresentativeCard;
 
 
 
