6d62ce2d30bd04cdada4ec07856f97304efce6c1
Calvin Dao <calvindao16@gmail.com>
2019-09-29 13:10:56 -0500

    removed phone numbers

--- src/Contact.js
+++ src/Contact.js
@@ -6,37 +6,31 @@ export const Contact = () => (
     <div>
       <h2>Abu-Zayd Abdullah</h2>
         <h3>&ensp;&ensp;&ensp;&ensp;Email: abuzayd.abdullah@gmail.com</h3>
-        <h3>&ensp;&ensp;&ensp;&ensp;Phone: </h3>
         <h3>&ensp;&ensp;&ensp;&ensp;GitLab: AbuZaydAbdullah</h3>
     </div>
     <div>
       <h2>Ben Burton</h2>
         <h3>&ensp;&ensp;&ensp;&ensp;Email: bentburton@gmail.com</h3>
-        <h3>&ensp;&ensp;&ensp;&ensp;Phone: </h3>
         <h3>&ensp;&ensp;&ensp;&ensp;GitLab: bentburton</h3>
     </div>
     <div>
       <h2>Ben Hazel</h2>
         <h3>&ensp;&ensp;&ensp;&ensp;Email: ben.hazel@gmail.com</h3>
-        <h3>&ensp;&ensp;&ensp;&ensp;Phone: </h3>
         <h3>&ensp;&ensp;&ensp;&ensp;GitLab: BenHazel007</h3>
     </div>
     <div>
       <h2>Calvin Dao</h2>
         <h3>&ensp;&ensp;&ensp;&ensp;Email: calvindao16@gmail.com</h3>
-        <h3>&ensp;&ensp;&ensp;&ensp;Phone: (512)971-4835</h3>
         <h3>&ensp;&ensp;&ensp;&ensp;GitLab: auproxis</h3>
     </div>
     <div>
       <h2>David Sikabwe</h2>
         <h3>&ensp;&ensp;&ensp;&ensp;Email: dsikabwe@utexas.edu</h3>
-        <h3>&ensp;&ensp;&ensp;&ensp;Phone: </h3>
         <h3>&ensp;&ensp;&ensp;&ensp;GitLab: dsikabwe</h3>
     </div>
     <div>
       <h2>Robert Gutierrez</h2>
         <h3>&ensp;&ensp;&ensp;&ensp;Email: rgutierrez27@utexas.edu</h3>
-        <h3>&ensp;&ensp;&ensp;&ensp;Phone: </h3>
         <h3>&ensp;&ensp;&ensp;&ensp;GitLab: robgutierrez1</h3>
     </div>
   </div>
