105d45805a8c81de03614ff644446418ac070bc8
Abu-ZaydAbdullah <hydrorain2012@hotmail.com>
2019-09-29 18:35:49 -0500

    gitlab: GitLab Integration added.

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
-import styled from 'styled-components';
 import data from "../data/GroupMembers"
+import axios from 'axios'
+import cloneDeep from 'lodash/cloneDeep';
 
 class AboutCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    commit_dict: {"Abu-Zayd Abdullah": 0, "Abu-ZaydAbdullah": 0, "Robert Gutierrez": 0, "Calvin Dao": 0, "David Sikabwe": 0, "Ben Burton": 0, "Ben Hazel": 0},
+    commit_count: 0,
+    issue_dict: {"Abu-Zayd Abdullah": 0, "Abu-ZaydAbdullah": 0, "Robert Gutierrez": 0, "Calvin Dao": 0, "David Sikabwe": 0, "Ben Burton": 0, "Ben Hazel": 0},
+    issue_count: 0
+    };
+  }
+
+  componentWillMount() {
+    
+    let getCommits = () => {
+      var authOptions = {
+        method: 'GET',
+        url: 'https://gitlab.com/api/v4/projects/14525540/repository/commits?all=true&per_page=100',
+        headers: {
+            'Private-Token': 'mS_bG_uQE4UkARaq3_oQ'
+        },
+        json: true
+      };
+      axios(authOptions)
+      .then(response => {
+        const responseData = response.data
+          responseData.forEach((commit) => {
+            this.setState({commit_count: this.state.commit_count += 1})
+            let temp_commit_dict = cloneDeep(this.state.commit_dict)
+            temp_commit_dict[commit.author_name] += 1
+            this.setState({commit_dict: temp_commit_dict})
+        })
+        let temp_commit_dict = cloneDeep(this.state.commit_dict)
+        temp_commit_dict['Abu-Zayd Abdullah'] += temp_commit_dict['Abu-ZaydAbdullah']
+        this.setState({commit_dict: temp_commit_dict})
+      })
+    }
+    
+    let getIssues = () => {
+      var authOptions = {
+        method: 'GET',
+        url: 'https://gitlab.com/api/v4/projects/14525540/issues?scope=all',
+        headers: {
+            'Private-Token': 'mS_bG_uQE4UkARaq3_oQ'
+        },
+        json: true
+      };
+      axios(authOptions)
+      .then(response => {
+        const responseData = response.data
+          responseData.forEach((issue) => {
+            this.setState({issue_count: this.state.issue_count += 1})
+            if (issue.closed_by != null) {
+            let temp_issue_dict = cloneDeep(this.state.issue_dict)
+            temp_issue_dict[issue.closed_by.name] += 1
+            this.setState({issue_dict: temp_issue_dict})
+            }
+        })
+        let temp_issue_dict = cloneDeep(this.state.issue_dict)
+        temp_issue_dict['Abu-Zayd Abdullah'] += temp_issue_dict['Abu-ZaydAbdullah']
+        this.setState({issue_dict: temp_issue_dict})
+      })
+    }
+    
+    getCommits()
+    getIssues()
+    console.log(this.state.issue_dict)
+    console.log(this.state.commit_dict)
+  }
+
   render() {
   const aboutList = data.map(member => {
   return (
@@ -15,6 +84,8 @@ class AboutCard extends Component {
       <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis feugiat elit, ac porta diam commodo non. In pulvinar neque dolor, sed vestibulum odio dapibus a. Curabitur turpis lacus, commodo eu purus sit amet, venenatis tristique ex. Donec sit amet mattis tellus, non hendrerit urna.</p>
       </p>
       {"\n"}
+      <p className="card-text"><strong>Commits:</strong> {this.state.commit_dict[member.name]}</p>
+      <p className="card-text"><strong>Issues:</strong> {this.state.issue_dict[member.name]}</p>
       <div className="d-flex justify-content-between align-items-center">
         <div className="btn-group"></div>
       </div>
--- src/data/GroupMembers.js
+++ src/data/GroupMembers.js
@@ -4,50 +4,61 @@ import abuzaydImage from '../assets/abuzayd.png'
 import benbImage from '../assets/benb.png'
 import benhImage from '../assets/benh.jpeg'
 import calvinImage from '../assets/calvin.jpg'
-
-
+import axios from 'axios'
 
 export default [
     {
       index: 0,
-      name: "Robert C. Gutierrez",
+      name: "Robert Gutierrez",
       gitlab_id: "robgutierrez1",
       email: "rgutierrez27@utexas.edu",
-      image: robertImage
+      image: robertImage,
+      // commits: commit_dict['Robert Gutierrez'],
+      // issues: issue_dict['Robert Gutierrez']
     },
     {
       index: 1,
       name: "Abu-Zayd Abdullah",
       gitlab_id: "AbuZaydAbdullah",
       email: "abuzaydabdullah@utexas.edu",
-      image: abuzaydImage
+      image: abuzaydImage,
+      // commits: commit_dict['Abu-Zayd Abdullah'],
+      // issues: issue_dict['Abu-Zayd Abdullah']
     },
     {
       index: 2,
       name: "David Sikabwe",
       gitlab_id: "dsikabwe",
       email: "dsikabwe@utexas.edu",
-      image: davidImage
+      image: davidImage,
+      // commits: commit_dict['David Sikabwe'],
+      // issues: issue_dict['David Sikabwe']
     },
     {
       index: 3,
       name: "Calvin Dao",
       gitlab_id: "auproxis",
       email: "calvindao16@gmail.com",
-      image: calvinImage
+      image: calvinImage,
+      // commits: commit_dict['Calvin Dao'],
+      // issues: issue_dict['Calvin Dao']
     },
     {
       index: 4,
       name: "Ben Hazel",
       gitlab_id: "BenHazel007",
       email: "ben.hazel@gmail.com",
-      image: benhImage
+      image: benhImage,
+      // commits: commit_dict['Ben Hazel'],
+      // issues: issue_dict['Ben Hazel']
     },
     {
       index: 5,
       name: "Ben Burton",
       gitlab_id: "bentburton",
       email: "bentburton@gmail.com",
-      image: benbImage
+      image: benbImage,
+      // commits: commit_dict['Ben Burton'],
+      // issues: issue_dict['Ben Burton']
     }
 ];
