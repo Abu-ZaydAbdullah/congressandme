f5da9b9f99f03279e30cfa5dab868e8cd504d1c6
Robert Gutierrez <robertg@cs.utexas.edu>
2019-10-01 17:57:03 -0500

    Added more flags.

--- src/data/StateData.js
+++ src/data/StateData.js
@@ -52,131 +52,163 @@ export default {
     },
     "10" : {
         index: 10,
-        name: "Hawaii"
+        name: "Hawaii",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Flag_of_Hawaii.svg/255px-Flag_of_Hawaii.svg.png"
     },
     "11" : {
         index: 11,
         name: "Idaho",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_Idaho.svg/216px-Flag_of_Idaho.svg.png"
     },
     "12" : {
         index: 12,
         name: "Illinois",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Flag_of_Illinois.svg/255px-Flag_of_Illinois.svg.png"
     },
     "13" : {
         index: 13,
         name: "Indiana",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Flag_of_Indiana.svg/255px-Flag_of_Indiana.svg.png"
     },
     "14" : {
         index: 14,
         name: "Iowa",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Flag_of_Iowa.svg/254px-Flag_of_Iowa.svg.png"
     },
     "15" : {
         index: 15,
-        name: "Kansas"
+        name: "Kansas",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Flag_of_Kansas.svg/255px-Flag_of_Kansas.svg.png"
     },
     "16" : {
         index: 16,
         name: "Kentucky",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Flag_of_Kentucky.svg/255px-Flag_of_Kentucky.svg.png"
     },
     "17" : {
         index: 17,
         name: "Louisiana",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Flag_of_Louisiana.svg/255px-Flag_of_Louisiana.svg.png"
     },
     "18" : {
         index: 18,
         name: "Maine",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Flag_of_Maine.svg/216px-Flag_of_Maine.svg.png"
     },
     "19" : {
         index: 19,
         name: "Maryland",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Flag_of_Maryland.svg/255px-Flag_of_Maryland.svg.png"
     },
     "20" : {
         index: 20,
-        name: "Massachusetts"
+        name: "Massachusetts",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Massachusetts.svg/255px-Flag_of_Massachusetts.svg.png"
     },
     "21" : {
         index: 21,
         name: "Michigan",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Flag_of_Michigan.svg/255px-Flag_of_Michigan.svg.png"
     },
     "22" : {
         index: 22,
         name: "Minnesota",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Minnesota.svg/255px-Flag_of_Minnesota.svg.png"
     },
     "23" : {
         index: 23,
         name: "Mississippi",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Flag_of_Mississippi.svg/200px-Flag_of_Mississippi.svg.png"
     },
     "24" : {
         index: 24,
         name: "Missouri",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Flag_of_Missouri.svg/200px-Flag_of_Missouri.svg.png"
     },
     "25" : {
         index: 25,
-        name: "Montana"
+        name: "Montana",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_Montana.svg/255px-Flag_of_Montana.svg.png"
     },
     "26" : {
         index: 26,
         name: "Nebraska",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Flag_of_Nebraska.svg/255px-Flag_of_Nebraska.svg.png"
     },
     "27" : {
         index: 27,
         name: "Nevada",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Flag_of_Nevada.svg/255px-Flag_of_Nevada.svg.png"
     },
     "28" : {
         index: 28,
         name: "New Hampshire",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Flag_of_New_Hampshire.svg/255px-Flag_of_New_Hampshire.svg.png"
     },
     "29" : {
         index: 29,
         name: "New Jersey",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_New_Jersey.svg/300px-Flag_of_New_Jersey.svg.png"
     },
     "30" : {
         index: 30,
-        name: "New Mexico"
+        name: "New Mexico",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_New_Mexico.svg/255px-Flag_of_New_Mexico.svg.png"
     },
     "31" : {
         index: 31,
         name: "New York",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_New_York.svg/255px-Flag_of_New_York.svg.png"
     },
     "32" : {
         index: 32,
         name: "North Carolina",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Flag_of_North_Carolina.svg/200px-Flag_of_North_Carolina.svg.png"
     },
     "33" : {
         index: 33,
         name: "North Dakota",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Flag_of_North_Dakota.svg/217px-Flag_of_North_Dakota.svg.png"
     },
     "34" : {
         index: 34,
         name: "Ohio",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Flag_of_Ohio.svg/255px-Flag_of_Ohio.svg.png"
     },
     "35" : {
         index: 35,
-        name: "Oklahoma"
+        name: "Oklahoma",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Flag_of_Oklahoma.svg/255px-Flag_of_Oklahoma.svg.png"
     },
     "36" : {
         index: 36,
         name: "Oregon",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Oregon.svg/255px-Flag_of_Oregon.svg.png"
     },
     "37" : {
         index: 37,
         name: "Pennsylvania",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Flag_of_Pennsylvania.svg/255px-Flag_of_Pennsylvania.svg.png"
     },
     "38" : {
         index: 38,
         name: "Rhode Island",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Rhode_Island.svg/185px-Flag_of_Rhode_Island.svg.png"
     },
     "39" : {
         index: 39,
         name: "South Carolina",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Flag_of_South_Carolina.svg/255px-Flag_of_South_Carolina.svg.png"
     },
     "40" : {
         index: 40,
-        name: "South Dakota"
+        name: "South Dakota",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_South_Dakota.svg/255px-Flag_of_South_Dakota.svg.png"
     },
     "41" : {
         index: 41,
         name: "Tennessee",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Tennessee.svg/255px-Flag_of_Tennessee.svg.png"
     },
     "42" : {
         index: 42,
