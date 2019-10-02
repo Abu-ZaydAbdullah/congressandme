86b6b0da4a0ab6b5b92822903e4d432f6e9d6fcf
Robert Gutierrez <robertg@cs.utexas.edu>
2019-10-02 13:04:59 -0500

    Resized flags to all be same size

--- src/components/StateCard.js
+++ src/components/StateCard.js
@@ -8,9 +8,9 @@ class StateCard extends Component {
     return (
   <div className="col-md-4" key={state.index}>
   <div className="card mb-4 box-shadow">
-    <img className="card-img-top" style={{maxHeight: 262}} src={state.image} alt="{state.name}"></img>
+    <img className="card-img-top" style={{maxHeight: 160}} src={state.image} alt="{state.name}"></img>
     <div className="card-img-overlay">
-      <div class="col-mb-4 text-center" style={{marginTop: "20%"}}>
+      <div class="col-mb-4 text-center" style={{marginTop: "12%"}}>
         <a href="#" class="btn btn-primary">{state.name}</a>
       </div>
       <div className="d-flex justify-content-between align-items-center">
