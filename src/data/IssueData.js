a19442d988fd64df0b7274c9ee366028dda4bd01
Calvin Dao <calvindao16@gmail.com>
2019-10-03 16:37:09 -0500

    updated issue about text

--- src/data/IssueData.js
+++ src/data/IssueData.js
@@ -12,7 +12,7 @@ export default {
              values a woman's bodily autonomy.",
       about: "Abortion is the act of euthanizing a developing fetus. It can be done for many reasons, ranging from\
               medical safety to the child being conceived through violence. While generally safe, some protest it\
-              because they believe the childs life should be preserved, regardless of what kind of circumstances\
+              because they believe the child's life should be preserved, regardless of what kind of circumstances\
               the child may be born into.",
       image: cancelImage,
       states: 34,
@@ -24,7 +24,7 @@ export default {
       name: "Environmentalism",
       desc: "Every day, the earth crawls closer to irreversible catastrophe thanks to our own actions. See what your politician\
              is doing to stop us from all burning alive here.",
-      about: "Climate change is the process by which green house gasses emitted by humans are heating the Earth up and\
+      about: "Climate change is the process by which greenhouse gasses emitted by humans are heating the Earth and\
               changing the expected weather pattens across the globe. Global temperatures are on the rise along with sea\
               levels and pollution count as humans continue to emit carbon into the air and sea. Some people try to deny\
               that this is happening by actively disagreeing with the 97% of scientists who agree that climate change is\
@@ -42,8 +42,8 @@ export default {
       about: "Guns are designed to do harm to other human beings. Some people argue that they need guns to protect themselves\
               while others would say that owning a gun is unecessary. Guns can do a lot of damage in a short amount of time,\
               so a big issue in the gun debate is whether the cost of hundreds of lives every year due to mass shootings\
-              is worth being able to own a high powered assault weapon for personal protection. Whether you advocate for\
-              gun confiscation programs or have a loose interpretation of the second ammendment the gun debate is a major\
+              is worth being able to own a high-powered assault weapon for personal protection. Whether you advocate for\
+              gun confiscation programs or have a loose interpretation of the second ammendment, the gun debate is a major\
               issue in America.",
       image: gunImage,
       states: 34,
@@ -56,7 +56,9 @@ export default {
       desc: "With the cost of medical care so high, some form of insurance is absolutely necessary to survival. See what your politician\
              thinks about subsidizing a basic neccesity for the masses.",
       about: "Healthcare is as expensive as it is necessary and costs are only rising. People go bankrupt every day because they\
-              can't afford to be sick or injured.",
+              can't afford to be sick or injured. Major injuries are left untreated because of the price, people choose to walk to the\
+              hospital or Uber rather then take an expensive ambulence. Healthcare helps subsidize these otherwise absurd costs and provide\
+              a basic quality of life to all citizens.",
       image: medicalImage,
       states: 44,
       rep: 8,
@@ -67,7 +69,9 @@ export default {
       name: "Immigration",
       desc: "America has a long and storied history of relying on immigrants to do much of the backbreaking labor... and despising them\
              for taking that work at the same time. See what your politican has been saying in this cycle of the fight.",
-      about: "Abortion",
+      about: "America is a melting pot, a nation built on the backs of immigrants. The influx of immigrants helps stimulate the economy, fill\
+              job positions generally considered unwanted, and help enrich our culture. However, negative perception of immigrants has led to\
+              law enforcement being tasked to round up undocumented immigrants, immigration requirements being raised, and more.",
       image: fenceImage,
       states: 44,
       rep: 8,
