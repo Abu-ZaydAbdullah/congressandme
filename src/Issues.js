10cf3876d3c0990740d37501e7b537841948ae0f
Calvin Dao <calvindao16@gmail.com>
2019-10-03 13:26:06 -0500

    fixed jumbotron on about page

--- src/Issues.js
+++ src/Issues.js
@@ -4,14 +4,8 @@ import IssueCard from './components/IssueCard'
 export const Issues = () => (
 
   <main role="main">
-
-    <section className="jumbotron text-center">
-      <div className="container">
-        <h1 className="jumbotron-heading">Issues</h1>
-        <p className="lead text-muted">"All the hottest topics being discussed"</p>
-      </div>
-    </section>
-
+    <Jumbotron title_text={"Issues"} subtitle_text={"All the hottest topics being discussed"}/>
+    <br></br>
     <div className="panel">
       <ul className="list-group" id="contact-list">
         <li className="list-group-item">
