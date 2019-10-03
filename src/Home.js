6c0628a490cce92e8fc0c0ce081363820ad6efb9
Ben Hazel <benhazel@cs.utexas.edu>
2019-10-03 13:50:23 -0500

    Added info on the home screen

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,15 @@ import Jumbotron from './components/Jumbotron'
 export const Home = () => (
   <main role="main">
     <div>
-    <Jumbotron title_text={"Congress and Me"} subtitle_text={"Welcome to the Home Page!"}/>
+      <Jumbotron title_text={"Congress and Me"} subtitle_text={"Welcome to the Home Page!"}/>
+    </div>
+    <h3 className="text-center" style={{marginBottom: '3%'}}>What is Congress and Me?</h3>
+    <div className="row" style={{marginBottom: '3%'}}>
+      <div className="col-md-3"></div>
+        <div className="col-md-6">
+          <h5 className="text-justify">The six of us believe that a well-informed populace is crucial to a functioning democracy. We wanted a way for people to easily see what issues their representatives are and aren’t talking about in their tweets and on the Congress floor, and we wanted to highlight which issues are being discussed and which ones require attention on a nation-wide scale. To that end, we’ve built Congress and Me.</h5>
+        </div>
+      <div className="col-md-3"></div>
     </div>
   </main>
 )
