9f7ea8266d6b8addeaa8041987a7777fa2fc52ab
David Sikabwe <dsikabwe@cs.utexas.edu>
2019-10-20 17:42:33 -0500

    Started on Flask appplication. Warning: app.py does not work.

--- backend/app.py
+++ backend/app.py
@@ -1,42 +1,30 @@
-import os
 from flask import Flask # noqa
 from flask_cors import CORS # noqa
 from flask_sqlalchemy import SQLAlchemy # noqa
 
 
-db = SQLAlchemy()
+db = flask_sqlalchemy.SQLAlchemy(app)
 
 """
 File to create a backend application
 """
 
-
 def create_app():
     """
     function to create and start the backend application (our api)
     """
-    app = Flask(__name__, static_folder='static')
-    CORS(app)
-    # set the database credentials from environment variables
-    # TA(s): ask us for the db credentials if you need  it
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://" + \
-        os.environ['DB_USER'] + ":" + os.environ['DB_PASS'] + "@" + \
-        os.environ['DB_HOST'] + "/" + os.environ['DB_NAME']
+    app = Flask(__name__)
+    CORS(app) 
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://cnmdevs:raredonkeyfart1@postgresql://cnmdatabase.c94ulbkdkv0l.us-east-1.rds.amazonaws.com:5432/mydatabase"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['DEBUG'] = True
-    # this is to avoid circular dependencies
-    from routes import rep_route, root_route, party_route, district_route, \
-        state_route, search_route
-    # register api endpoints
-    app.register_blueprint(root_route, url_prefix='/')
-    app.register_blueprint(rep_route, url_prefix='/representative')
-    app.register_blueprint(party_route, url_prefix='/party')
-    app.register_blueprint(district_route, url_prefix='/district')
-    app.register_blueprint(state_route, url_prefix='/state')
-    app.register_blueprint(search_route, url_prefix='/search')
-    db.init_app(app)
+    db = flask_sqlalchemy.SQLAlchemy(app)
+
     import models # noqa
     with app.app_context():
         db.configure_mappers()
         db.create_all(app=app)
     return app
+
+if __name__ == '__main__':
+    app.run()
