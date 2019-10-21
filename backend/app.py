15261c206137cb1697fb2dfcf9241b841a28201b
Robert Gutierrez <robgutierrez@Roberts-MacBook-Pro.local>
2019-10-20 21:13:00 -0500

    Created APIs for models using flask-restless.

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,60 @@
-from flask import Flask # noqa
-from flask_cors import CORS # noqa
-from flask_sqlalchemy import SQLAlchemy # noqa
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
+
+# Handles the creation of the Flask app
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+app.config['DEBUG'] = True
+
+# Flask Alchemy obj
+db = SQLAlchemy(app)
+
+# Create the Flask-SQLAlchemy models.
+class Representatives(db.Model):
+    __tablename__ =     'Representatives'
+    full_name =         db.Column(db.Unicode, primary_key = True)
+    Type =              db.Column(db.Unicode)
+    state =             db.Column(db.Unicode)
+    district =          db.Column(db.Unicode)
+    senate_class =      db.Column(db.Unicode)
+    party =             db.Column(db.Unicode)
+    url =               db.Column(db.Unicode)
+    phone =             db.Column(db.Unicode)
+    contact_form =      db.Column(db.Unicode)
+    rss_url =           db.Column(db.Unicode)
+    twitter =           db.Column(db.Unicode)
+    facebook =          db.Column(db.Unicode)
+    youtube =           db.Column(db.Unicode)
+    issues =            db.Column(db.Unicode)
+    bioguide_id =       db.Column(db.Unicode)
+    bioguide_summary =  db.Column(db.Unicode)
+
+class States(db.Model):
+    __table__name =     'States'
+    abbreviation =      db.Column(db.Unicode, primary_key = True)
+    name =              db.Column(db.Unicode)
+    image =             db.Column(db.Unicode)
+    website =           db.Column(db.Unicode)
+    summary =           db.Column(db.Unicode)
+
+class Issues(db.Model):
+    __table__name =     'Issues'
+    name =              db.Column(db.Unicode, primary_key = True)
+    desc =              db.Column(db.Unicode)
+    about =             db.Column(db.Unicode)
+    image =             db.Column(db.Unicode)
+    vids =              db.Column(db.Unicode)
+
+# Allows for the use of Flask-Restless to quickly build APIs
+# Requires app we just built with configurations and a flask alchemy obj
+manager = APIManager(app, flask_sqlalchemy_db=db)
+
+# Create APIs for our above models. Will be used to send and receive messages in JSON format
+manager.create_api(Representatives, methods=['GET'], max_results_per_page=10)
+manager.create_api(States, methods=['GET'], max_results_per_page=10)
+manager.create_api(Issues, methods=['GET'], max_results_per_page=10)
 
 
-db = flask_sqlalchemy.SQLAlchemy(app)
-
-"""
-File to create a backend application
-"""
-
-def create_app():
-    """
-    function to create and start the backend application (our api)
-    """
-    app = Flask(__name__)
-    CORS(app) 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://cnmdevs:raredonkeyfart1@postgresql://cnmdatabase.c94ulbkdkv0l.us-east-1.rds.amazonaws.com:5432/mydatabase"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['DEBUG'] = True
-    db = flask_sqlalchemy.SQLAlchemy(app)
-
-    import models # noqa
-    with app.app_context():
-        db.configure_mappers()
-        db.create_all(app=app)
-    return app
-
-if __name__ == '__main__':
-    app.run()
+if __name__=='__main__':
+	app.run(host='0.0.0.0', port=80)
