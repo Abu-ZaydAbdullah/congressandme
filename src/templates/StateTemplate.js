c078e4f1cc932c001dd197f9ca4d0a2d575dfa9e
Robert Gutierrez <robertg@cs.utexas.edu>
2019-10-25 17:56:05 -0500

    Need to fix map issue with statetemplate

--- src/templates/StateTemplate.js
+++ src/templates/StateTemplate.js
@@ -13,20 +13,79 @@ class StateTemplate extends React.Component {
                 image: "",
                 website: "",
                 summary: ""
+            },
+
+            issue_data : {
+                name: "",
+                description: "",
+                about: "",
+                image: "",
+                vids: ""
+            }, 
+
+            mentions_data : {
+                full_name: "",
+                state: "",
+                agriculture: "",
+                armed_forces: "",
+                crimes: "",
+                economics: "",
+                education: "",
+                emergency_management: "",
+                environmentalism: "",
+                gun_control: "",
+                healthcare: "",
+                housing: "",
+                immigration: "",
+                labor: "",
+                social_issues: "",
+                taxation: "",
+                transportation_and_public_works: "",
+                others: ""
+            },
+
+            rep_data : {
+                full_name: "",
+                chamber: "",
+                state : "",
+                district: "",
+                senate_class: "",
+                party: "",
+                url: "",
+                phone: "",
+                contact_form: "",
+                rss_url: "",
+                twitter: "",
+                facebook: "",
+                youtube: "",
+                issues: "",
+                bioguide_id: "",
+                bioguide_summary: ""
             }
         }
     }
 
     componentDidMount()
     {
-      this.fetchReps()
+      this.fetchData()
     }
   
-    fetchReps = async() => {
-      //console.log(this.props.page_num)
-      let res = await axios(`https://api.congressand.me/api/States?page=${this.props.page_num}`)
-      await this.setState({state_data : res.data.objects[parseInt(this.props.index)]});
-      await console.log(this.state.state_data)
+    fetchData = async() => {
+        //console.log(this.props.page_num)
+        let res = await axios(`https://api.congressand.me/api/States?page=${this.props.page_num}`)
+        await this.setState({state_data : await res.data.objects[parseInt(this.props.index)]});
+        await console.log(this.state.state_data)
+        let res2 = await axios(`https://api.congressand.me/api/Mentions?q={"filters":[{"name":"state","op":"eq","val":"${this.state.state_data.abbreviation}"}]}`)
+        await this.setState({mentions_data : await res2.data.objects})
+        await console.log(this.state.mention_data)
+        let res3 = await axios(`https://api.congressand.me/api/Issues?page=${this.props.page_num}`)
+        await this.setState({issue_data : await res3.data.objects[parseInt(this.props.index)]});
+        await console.log(this.state.issue_data)
+        console.log(this.state.state_data.abbreviation)
+        let res4 = await axios(`https://api.congressand.me/api/Representatives?q={"filters":[{"name":"state","op":"eq","val":"${this.state.state_data.abbreviation}"}]}`)
+        console.log(res4)
+        await this.setState({rep_data : await res4.data.objects})
+        await console.log(this.state.rep_data)
     }
 
     render() {
@@ -84,120 +143,134 @@ class StateTemplate extends React.Component {
                 </div>
                 <div className="row mb-5">
                     <h1>The Following Issues are Important in this State:</h1>
+                    <div className="panel panel-default">
+                        <div className="card-body row">    
+                            <div class = "col-sm-6 col-md-6 image-container">
+                                <img className="card-img-top about-image" style={{width: 262}} src={this.state.issue_data.image} alt="Card image cap"></img>
+                            </div>
+                            <div className="col-sm-6 col-md-6" >
+                                <h5>{this.state.issue_data.name}</h5>
+                                <p className="card-text">{this.state.issue_data.desc}</p>
+                                <Link
+                                to={{
+                                    pathname: `/issue/${this.state.issue_data.name}/1/0`
+                                }}
+                                >
+                                <a class="btn btn-dark">{this.state.issue_data.name}</a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="row mb-5">
                     <h1>This State's Representatives!</h1>
-                </div>
-            </div>
-                /* <div className="row mb-5">
-                <h1>The Following Issues are Important in this State:</h1>
-                <div className="panel panel-default">
-                    <div className="card-body row">    
-                        <div class = "col-sm-6 col-md-6 image-container">
-                            <img className="card-img-top about-image" style={{width: 262}}src={member.image} alt="Card image cap"></img>
-                        </div>
-                        <div className="col-sm-6 col-md-6" >
-                            <h5>{member.name}</h5>
-                            <p className="card-text">{member.desc}</p>
+                    {this.state.rep_data.map((representative, index) => {
+                        return(
+                            <div className="col-md-4">
+                            <div className="card mb-4 box-shadow">
                             <Link
-                            to={{
-                                pathname: `/issue/${props.location.state..issues}`,
-                                status: {
-                                name: member.name,
-                                desc: member.desc,
-                                image: member.image,
-                                states: member.states,
-                                reps: member.rep,
-                                vids: member.vids,
-                                }
-                            }}
-                            >
-                            <a class="btn btn-dark">{member.name}</a>
-                            </Link>
-                        </div>
-                    </div>
+                                to={{
+                                  pathname: `/representative/${index}`,
+                                  state: {
+                                    name: representative.full_name,
+                                    chamber:
+                                      representative.type === "sen"
+                                        ? "Senate"
+                                        : "House of Representatives",
+                                    image: `https://github.com/Abu-ZaydAbdullah/images/raw/gh-pages/congress/450x550/${representative.bioguide_id}.jpg`,
+                                    party: representative.party,
+                                    state: representative.state,
+                                    twitter: `https://twitter.com/@${representative.twitter}`,
+                                    facebook: `https://facebook.com/${representative.facebook}`,
+                                    youtube: `https://www.youtube.com/results?search_query=${representative.youtube}`,
+                                    rss: representative.rss_url,
+                                    website: representative.url,
+                                    form: representative.contact_form,
+                                    phone: `tel:${representative.phone}`,
+                                    summary: representative.bioguide_summary
+                                  }
+                                }}
+                              >
+                                <img
+                                  className="card-img-top about-image"
+                                  style={{ maxHeight: 450 }}
+                                  src={`https://github.com/Abu-ZaydAbdullah/images/raw/gh-pages/congress/450x550/${representative.bioguide_id}.jpg`}
+                                  alt={representative.full_name}
+                                ></img>
+                              </Link>
+                              
+                              <div className="card-body">
+                                <h5>{representative.full_name}</h5>
+                                <p className="card-text">
+                                  <strong>Party:</strong> {representative.party}
+                                </p>
+                                <p className="card-text">
+                                  <strong>State:</strong> {representative.state}
+                                </p>
+                                <p className="card-text">
+                                  <strong>Chamber:</strong>{" "}
+                                  {representative.chamber === "sen"
+                                    ? "Senate"
+                                    : "House of Representatives"}
+                                </p>
+                                <p className="card-text">
+                                  <strong>
+                                    <a href={`https://twitter.com/@${representative.twitter}`}>
+                                      Twitter:
+                                    </a>
+                                  </strong>{" "}
+                                  {representative.twitter !== ""
+                                    ? `@${representative.twitter}`
+                                    : "n/a"}
+                                </p>
+                                <p className="card-text">
+                                  <strong>
+                                    <a href={`https://facebook.com/${representative.facebook}`}>
+                                      Facebook:
+                                    </a>
+                                  </strong>{" "}
+                                  {representative.facebook !== ""
+                                    ? `${representative.facebook}`
+                                    : "n/a"}
+                                </p>
+                                <div class="col-mb-4 text-center">
+                                  <Link
+                                    to={{
+                                      pathname: `/representative/${index}`,
+                                      state: {
+                                        name: representative.full_name,
+                                        chamber:
+                                          representative.type === "sen"
+                                            ? "Senate"
+                                            : "House of Representatives",
+                                        image: `https://github.com/Abu-ZaydAbdullah/images/raw/gh-pages/congress/450x550/${representative.bioguide_id}.jpg`,
+                                        party: representative.party,
+                                        state: representative.state,
+                                        twitter: `https://twitter.com/@${representative.twitter}`,
+                                        facebook: `https://facebook.com/${representative.facebook}`,
+                                        youtube: `https://www.youtube.com/results?search_query=${representative.youtube}`,
+                                        rss: representative.rss_url,
+                                        website: representative.url,
+                                        form: representative.contact_form,
+                                        phone: `tel:${representative.phone}`,
+                                        summary: representative.bioguide_summary
+                                      }
+                                    }}
+                                  >
+                                    <a class="btn btn-light">Learn More</a>
+                                  </Link>
+                                </div>
+                                <div className="d-flex justify-content-between align-items-center">
+                                  <div className="btn-group"></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                    })}
+                
                 </div>
-            </div> */
-            /* <h1>This State's Representatives!</h1>
-            <div className="col-md-4" key={representative.index}>
-                <div className="card mb-4 box-shadow">
-                    <img
-                    className="card-img-top about-image"
-                    style={{ maxHeight: 450 }}
-                    src={`https://github.com/Abu-ZaydAbdullah/images/raw/gh-pages/congress/450x550/${representative.bioguide_id}.jpg`}
-                    alt="{representative.full_name}"
-                    ></img>
-                    <div className="card-body">
-                    <h5>{representative.full_name}</h5>
-                    <p className="card-text">
-                        <strong>Party:</strong> {representative.party}
-                    </p>
-                    <p className="card-text">
-                        <strong>State:</strong> {representative.state}
-                    </p>
-                    <p className="card-text">
-                        <strong>Chamber:</strong>{" "}
-                        {representative.type === "sen"
-                        ? "Senate"
-                        : "House of Representatives"}
-                    </p>
-                    <p className="card-text">
-                        <strong>
-                        <a href={`https://twitter.com/@${representative.twitter}`}>
-                            Twitter:
-                        </a>
-                        </strong>{" "}
-                        {representative.twitter !== ""
-                        ? `@${representative.twitter}`
-                        : "n/a"}
-                    </p>
-                    <p className="card-text">
-                        <strong>
-                        <a href={`https://facebook.com/${representative.facebook}`}>
-                            Facebook:
-                        </a>
-                        </strong>{" "}
-                        {representative.facebook !== ""
-                        ? `${representative.facebook}`
-                        : "n/a"}
-                    </p>
-                    <div class="col-mb-4 text-center">
-                        <Link
-                        to={{
-                            pathname: `/representative/${representative.full_name}`,
-                            state: {
-                            name: representative.full_name,
-                            chamber:
-                                representative.type === "sen"
-                                ? "Senate"
-                                : "House of Representatives",
-                            type:
-                                representative.type === "sen"
-                                ? "Senator"
-                                : "Representative",
-                            image: `https://github.com/Abu-ZaydAbdullah/images/raw/gh-pages/congress/450x550/${representative.bioguide_id}.jpg`,
-                            party: representative.party,
-                            state: representative.state,
-                            twitter: `https://twitter.com/@${representative.twitter}`,
-                            facebook: `https://facebook.com/${representative.facebook}`,
-                            youtube: `https://www.youtube.com/results?search_query=${representative.youtube}`,
-                            rss: representative.rss_url,
-                            website: representative.url,
-                            form: representative.contact_form,
-                            phone: `tel:${representative.phone}`,
-                            summary: representative.bioguide_summary
-                            }
-                        }}
-                        >
-                        <a class="btn btn-light">Learn More</a>
-                        </Link>
-                    </div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <div className="btn-group"></div>
-                    </div>
-                    </div> */
-                /* </div> */
-            /* </div> */
+            </div> 
           );
         }
     }
