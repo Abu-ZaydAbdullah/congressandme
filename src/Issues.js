51ef34b6a4a64cd7ee31dd11337e42aedfad4efa
Ben Hazel <benhazel@cs.utexas.edu>
2019-11-06 19:01:22 -0600

    sorting alphabetically on all model pages [ci skip]

--- src/Issues.js
+++ src/Issues.js
@@ -3,22 +3,79 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import Jumbotron from "./components/Jumbotron";
 import issueImage from "./assets/issueImage.jpg";
+import { Dropdown } from "react-bootstrap"
 
 function Issues() {
   const [issues, setIssues] = useState([]);
   const { page_num } = useParams();
+  const [dataSize, setDataSize] = useState(5);
+  const [sort_dir, setSortDir] = useState("A-Z");
+  const [data, setData] = useState([]);
 
   const fetchIssues = async () => {
-    let res = await axios(
-      `https://api.congressand.me/api/Issues?page=${page_num}`
+    if(data.length == 0) {
+      let res = await axios(
+      `https://api.congressand.me/api/Issues?results_per_page=15`
     );
     let data = await res.data.objects;
-    await setIssues(data);
+    const start_index = (page_num - 1)*5
+    await setData(data);
+    await setIssues(data.slice(start_index, start_index + 5));
+    await setDataSize(data.length);
+    } else {
+      const start_index = (page_num - 1)*5
+      setData(data);
+      setIssues(data.slice(start_index, start_index + 5));
+      setDataSize(data.length);
+    }
   };
 
   useEffect(() => {
     fetchIssues();
   }, [page_num]);
+
+  const sortIssues = () => {
+    if(sort_dir == "A-Z")
+    {
+      const start_index = (page_num - 1)*5
+      setIssues(data.sort(function(a, b){
+        if(a.name < b.name) { return -1; }
+        if(a.name > b.name) { return 1; }
+        return 0;
+    }).slice(start_index, start_index + 5))
+    } else if (sort_dir == "Z-A") {
+      const start_index = (page_num - 1)*5
+      setIssues(data.sort(function(a, b){
+        if(a.name > b.name) { return -1; }
+        if(a.name < b.name) { return 1; }
+        return 0;
+    }).slice(start_index, start_index + 5))
+    }
+  };
+
+  useEffect(() => {
+    sortIssues();
+  }, [sort_dir]);
+
+  const pagination_list = () => {
+    let p_list = [];
+    for(var i = 0; i < dataSize/5; i++)
+    {
+      p_list.push(
+      <li class="page-item">
+        <Link
+          to={{
+            pathname: `/issues/page/${i + 1}`,
+            state: { page_num: i + 1 }
+          }}
+        >
+          <a class="page-link">{i + 1}</a>
+        </Link>
+      </li>
+      )
+    }
+    return p_list;
+  }
 
   const issueList = issues.map(issue => {
     console.log(issue.abbreviation);
@@ -80,6 +137,21 @@ function Issues() {
         />
         <br></br>
         <h1 className="page-title">Issues</h1>
+        <div className="row">
+          <div className="col-md-10"></div>
+          <div className="col-md-2">
+          <Dropdown>
+            <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
+              Sort By:
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => {setSortDir("A-Z");}}>A-Z</Dropdown.Item>  
+              <Dropdown.Item onClick={() => {setSortDir("Z-A");}}>Z-A</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          </div>
+          </div>
         <div className="panel">
           <ul className="list-group" id="contact-list">
             <li className="list-group-item">
@@ -94,36 +166,7 @@ function Issues() {
                         <div className="col-md-4">
                           <nav>
                             <ul aria-label="Page:" class="pagination">
-                              <li class="page-item">
-                                <Link
-                                  to={{
-                                    pathname: `/issues/page/1`,
-                                    state: { page_num: 1 }
-                                  }}
-                                >
-                                  <a class="page-link">1</a>
-                                </Link>
-                              </li>
-                              <li class="page-item">
-                                <Link
-                                  to={{
-                                    pathname: `/issues/page/2`,
-                                    state: { page_num: 2 }
-                                  }}
-                                >
-                                  <a class="page-link">2</a>
-                                </Link>
-                              </li>
-                              <li class="page-item">
-                                <Link
-                                  to={{
-                                    pathname: `/issues/page/3`,
-                                    state: { page_num: 3 }
-                                  }}
-                                >
-                                  <a class="page-link">3</a>
-                                </Link>
-                              </li>
+                              {pagination_list()}
                             </ul>
                           </nav>
                         </div>
