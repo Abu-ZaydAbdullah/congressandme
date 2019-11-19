c81bbb1c3924aebf46973af4698a8337d5deaab4
Abu-ZaydAbdullah <hydrorain2012@hotmail.com>
2019-11-18 21:42:11 -0600

    npm: Pruning uneeded packages.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,40 +1,16 @@
 import React, { Component } from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
-import styled from "styled-components";
 
 class Jumbotron extends Component {
   render() {
-    const Styles = styled.div`
-      .jumbo {
-        background: url(${this.props.image}) no-repeat fixed bottom;
-        background-size: contain;
-        color: #efefef;
-        height: 500px;
-        position: relative;
-        z-index: -2;
-      }
-
-      .overlay {
-        background-color: #000;
-        opacity: 0.6;
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        z-index: -1;
-      }
-    `;
-    return (
-      <Styles>
-        <Jumbo fluid className="jumbo">
+    return (  
+        <Jumbo fluid className="jumbo" style={{background: `url(${this.props.image})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
           <div className="overlay"></div>
           <Container>
             <h1 className="text-center">{this.props.title_text}</h1>
             <p className="text-center">{this.props.subtitle_text}</p>
           </Container>
         </Jumbo>
-      </Styles>
     );
   }
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,50 +1,30 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
-import styled from "styled-components";
-
-const Styles = styled.div`
-  .navbar {
-    background-color: #007bff;
-  }
-
-  a,
-  .navbar-brand,
-  .navbar-nav .nav-link {
-    color: #fdfdfd;
-    font-weight: 500;
-
-    &:hover {
-      color: white;
-    }
-  }
-`;
 
 export const NavigationBar = () => (
-  <Styles>
-    <Navbar expand="lg">
-      <Navbar.Brand href="/">Congress and Me</Navbar.Brand>
+    <Navbar expand="lg" style={{backgroundColor: "#007bff"}}>
+      <Navbar.Brand href="/" style={{color: "#fdfdfd", fontWeight: '500'}}>Congress and Me</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item>
-            <Nav.Link href="/" className=".HomeLink">
+            <Nav.Link href="/" className=".HomeLink" style={{color: "#fdfdfd", fontWeight: '500'}}>
               Home
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/representatives/page/1">Representatives</Nav.Link>
+            <Nav.Link href="/representatives/page/1" style={{color: "#fdfdfd", fontWeight: '500'}}>Representatives</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/states/page/1">States</Nav.Link>
+            <Nav.Link href="/states/page/1" style={{color: "#fdfdfd", fontWeight: '500'}}>States</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/issues/page/1">Issues</Nav.Link>
+            <Nav.Link href="/issues/page/1" style={{color: "#fdfdfd", fontWeight: '500'}}>Issues</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/about" style={{color: "#fdfdfd", fontWeight: '500'}}>About</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  </Styles>
 );
