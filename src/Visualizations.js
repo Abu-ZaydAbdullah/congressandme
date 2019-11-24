fd87d915cd1084447bf02415c95189cfde265696
David Sikabwe <dsikabwe@cs.utexas.edu>
2019-11-24 17:39:14 -0600

    Working on viz. Does not work. Blah. [ci skip]

--- src/Visualizations.js
+++ src/Visualizations.js
@@ -1,6 +1,9 @@
-import React, { lazy } from "react";
+import React, { lazy, useRef, useEffect } from "react";
+import PieChart from "./components/PieChart"
+import * as d3 from "d3";
 
-function Visualizations() {
+function Visualizations () {
+  
   return (
     <main role="main">
     <section className="jumbotron text-center">
@@ -11,8 +14,20 @@ function Visualizations() {
         </p>
       </div>
     </section>
+    
+    <div className="album py-5 bg-light">
+        <h3 className="text-center" style={{ marginBottom: "3%" }}>
+          Visualization 1
+        </h3>
+
+            <div className="col-md-6">
+            <PieChart x={100} y={100} outerRadius={100} innerRadius={50}
+          data={[{value: 92-34, label: 'Code lines'},
+                 {value: 34, label: 'Empty lines'}]} />
+            </div>
+      </div>
     </main>
-  )
+  );
 }
 
 export default Visualizations;
