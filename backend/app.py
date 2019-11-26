cba5b26b0567c63d7847e0267fafd64e3ee0705e
Abu-ZaydAbdullah <hydrorain2012@hotmail.com>
2019-11-25 21:10:05 -0600

    api: Updated api.py

--- backend/app.py
+++ backend/app.py
@@ -147,9 +147,9 @@ manager.create_api(Representatives, methods=['GET'], results_per_page=54, max_re
 manager.create_api(States, methods=['GET'], results_per_page=10, max_results_per_page=51)
 manager.create_api(Issues, methods=['GET'], results_per_page=5, max_results_per_page=30)
 manager.create_api(Mentions, methods = ['GET'], results_per_page=0)
-manager.create_api(megaTable, methods = ['GET'])
-manager.create_api(stateIssues, methods = ['GET'])
-manager.create_api(Tweets, methods = ['GET'])
+manager.create_api(megaTable, methods = ['GET'], max_results_per_page=540)
+manager.create_api(stateIssues, methods = ['GET'], max_results_per_page=540)
+manager.create_api(Tweets, methods = ['GET'], max_results_per_page=2000)
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
